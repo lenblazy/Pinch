26054d395532a9ab7a48bd46153f53097f1dcccd
lenblazy <lenblazy@gmail.com>
2023-06-02 11:51:41 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Lennox Mwabonje on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
