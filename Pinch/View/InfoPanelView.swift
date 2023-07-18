f3758bcb5d591438ecfaaebccfa8283530ba3f1b
lenblazy <lenblazy@gmail.com>
2023-07-18 11:38:26 +0300

    Add InfoPanelView

---
+++ Pinch/View/InfoPanelView.swift
@@ -0,0 +1,59 @@
+//
+//  InfoPanelView.swift
+//  Pinch
+//
+//  Created by Lennox Mwabonje on 06/07/2023.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    
+    var scale: CGFloat
+    var offset: CGSize
+    
+    var body: some View {
+        HStack{
+            //MARK: - HOTSPOT
+            Image(systemName: "circle.circle")
+                .symbolRenderingMode(.hierarchical)
+                .resizable()
+                .frame(width: 30, height: 30)
+            
+            Spacer()
+            
+            //MARK: - INFO PANEL
+            HStack(spacing: 2) {
+                Image(systemName: "arrow.up.left.and.arrow.down.right")
+                Text("\(scale)")
+                Spacer()
+                
+                Image(systemName: "arrow.left.and.right")
+                Text("\(offset.width)")
+                Spacer()
+                
+                Image(systemName: "arrow.up.and.down")
+                Text("\(offset.height)")
+                Spacer()
+                
+            }
+            .font(.footnote)
+            .padding(8)
+            .background(.ultraThinMaterial)
+            .cornerRadius(8)
+            .frame(maxWidth: 420)
+            
+            Spacer()
+            
+        }
+    }
+}
+
+struct InfoPanelView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoPanelView(scale: 1.0, offset: .zero)
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
