6188ff220f975b6cca13a397ae80dd4244caeae6
lenblazy <lenblazy@gmail.com>
2023-07-18 12:33:07 +0300

    Add action to drawer

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
+    @State private var isDrawerOpen: Bool = false
+    
     
     //MARK: - FUNCTION
     private func resetImageState() {
@@ -155,12 +157,17 @@ struct ContentView: View {
             .overlay(alignment: .topTrailing) {
                 HStack(spacing: 12) {
                     //MARK: - DRAWER HANDLE
-                    Image(systemName: "chevron.compact.left")
+                    Image(systemName: isDrawerOpen ? "chevron.compact.right" : "chevron.compact.left")
                         .resizable()
                         .scaledToFit()
                         .frame(height: 40)
                         .padding(8)
                         .foregroundColor(.secondary)
+                        .onTapGesture {
+                            withAnimation(.easeOut) {
+                                isDrawerOpen.toggle()
+                            }
+                        }
                     
                     //MARK: - THUMBNAILS
                     Spacer()
@@ -172,6 +179,7 @@ struct ContentView: View {
                 .opacity(isAnimating ? 1 : 0)
                 .frame(width: 260)
                 .padding(.top, UIScreen.main.bounds.height / 12)
+                .offset(x: isDrawerOpen ? 20 : 215)
             }
             
         } //: NAVIGATION
