f4d328a2a1dd21c3eebec30fdf392c9354e7fbad
lenblazy <lenblazy@gmail.com>
2023-07-18 12:04:59 +0300

    Add controls buttons

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -81,6 +81,38 @@ struct ContentView: View {
                     .padding(.top, 30)
             })
             
+            //MARK: - Overlay
+            .overlay(alignment: .bottom) {
+                Group{
+                    HStack {
+                        // SCALE DOWN
+                        Button {
+                            //some action
+                        } label: {
+                            ControlImageView(icon: "minus.magnifyingglass")
+                        }
+                        // RESET
+                        Button {
+                            //some action
+                        } label: {
+                            ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        }
+                        // SCALE UP
+                        Button {
+                            //some action
+                        } label: {
+                            ControlImageView(icon: "plus.magnifyingglass")
+                        }
+                        
+                    }//: CONTROLS
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                    
+                }
+                .padding(.bottom, 30)
+            }
             
         } //: NAVIGATION
         .navigationViewStyle(.stack)
